bdf26357ef37a1e19eca5c7ac22946cb3e912b56
Azamjon0898 <98kevin08@gmail.com>
2024-12-10 20:34:03 +0900

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -12,3 +12,12 @@
 // MVC = MODEL VIEW CONTROLLER
 
 // Design pattern: Middleware, Decator
+
+// import moment from "moment"; // const moment require("moment");
+
+import dotenv from "dotenv";
+dotenv.config();
+
+// console.log("PORT:", process.env.PORT);
+
+// console.log("MONGO_URL", process.env.MONGO_URL);
