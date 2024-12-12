f2d68bf49c0f72af1f14597d5b9a5ec2fdea2337
Azamjon0898 <98kevin08@gmail.com>
2024-12-12 20:04:02 +0900

    feat: develop member schema model

---
+++ src/schema/Member.model.ts
@@ -0,0 +1,54 @@
+import mongoose, {Schema} from "mongoose";
+import { memberStatus, MemberType } from "../libs/enums/member.enum";
+
+
+
+const memberSchema = new Schema({
+    memberType: {
+        type: String,
+        enum: MemberType,
+        default: MemberType.USER
+    },
+
+    memberStatus: {
+        type: String,
+        enum: memberStatus,
+        default: memberStatus.ACTIVE
+    },
+
+    memberNick: {
+        type: String,
+        index: { unique: true, sparse: true} ,
+        required: true,
+    },
+
+    memberPhone: {
+        type: String,
+        index:  { unique: true, sparse: true },
+        required: true
+    },
+
+    memberPassword: {
+        type: String,
+        select: false,
+        required: true,
+    },
+
+    memberAdress: {
+        type: String,
+    },
+
+    memberDesc: {
+        type: String,
+    },
+
+    
+    
+    memberImage: {
+        type: Number,
+        default: 0,
+    },
+}, { timestamps: true }      // updatedAt, createdAt
+);
+
+export default mongoose.model("Member", memberSchema);
--- src/train.js
+++ src/train.js
@@ -1,15 +1,30 @@
 console.log("Train Area:");
 
-// H-TASK
+// // H-TASK
 
-function getPositive(arr) {
-    return arr.filter(num => num > 0).join('');
-  }
+// function getPositive(arr) {
+//     return arr.filter(num => num > 0).join('');
+//   }
   
-  // Misol uchun
-  console.log(getPositive([1, -4, 2]));
-  console.log(getPositive([1, -4, 2, 5, -10]));
-  console.log(getPositive([-1, -16, 6, 27]));
-  console.log(getPositive([-19, -21,, 9, 101, 30]));
+//   // Misol uchun
+//   console.log(getPositive([1, -4, 2]));
+//   console.log(getPositive([1, -4, 2, 5, -10]));
+//   console.log(getPositive([-1, -16, 6, 27]));
+//   console.log(getPositive([-19, -21,, 9, 101, 30]));
 
+
+// H-2 TASK
+
+function getDigits(inputString) {
+  return inputString.split('').filter(char => /\d/.test(char)).join('');
+}
+
+const result = getDigits("m14i1t");
+console.log(result);
+const result2 = getDigits("m2456ion5jb5t");
+console.log(result2);
+const result3 = getDigits("n784n4j4n34h459b");
+console.log(result3);
+const result4 = getDigits("m8m34k4n9b");
+console.log(result4);
   
