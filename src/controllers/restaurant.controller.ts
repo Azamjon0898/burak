5125259fdf839041c65abe699c5218909662ca1f
Azamjon0898 <98kevin08@gmail.com>
2024-12-24 16:33:21 +0900

    feat: create checkAuthSession api

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -3,6 +3,7 @@ import { T } from "../libs/types/common";
 import MemberService from "../models/Member.service";
 import { AdminRequest, LoginInput, MemberInput } from "../libs/types/member";
 import { MemberType } from "../libs/enums/member.enum";
+import { Message } from "../libs/Errors";
 
 const memberService = new MemberService();
 
@@ -52,17 +53,35 @@ restaurantController.processSignup = async (req: AdminRequest, res: Response) =>
   }
 };
 
-restaurantController.processLogin = async (req: AdminRequest, res: Response) => {
+restaurantController.processLogin = async (
+  req: AdminRequest, 
+  res: Response
+) => {
   try {
-    console.log("processLogin");
+     console.log("processLogin");
 
-    const input: LoginInput = req.body;
-    const result = await memberService.processLogin(input);
+     const input: LoginInput = req.body;
+     const result = await memberService.processLogin(input);
 
-req.session.member = result;
+      req.session.member = result;
       req.session.save(function() {
         res.send(result);
       });
+  } catch(err) {
+      console.log("Error, processLogin:", err);
+      res.send(err);
+  }
+};
+
+restaurantController.checkAuthSession = async (
+  req: AdminRequest, 
+  res: Response
+) => {
+  try {
+     console.log("checkAuthSession");
+     if(req.session?.member) 
+      res.send(`<script> alert("${req.session.member.memberNick}") </script>`);
+     else res.send(`<script> alert("${Message.NOT_AUTHENTICATED}") </script>`);
   } catch(err) {
       console.log("Error, processLogin:", err);
       res.send(err);
