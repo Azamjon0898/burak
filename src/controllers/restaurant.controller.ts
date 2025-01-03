cacc97f6713048bc49ec10b3232ee6fe1c42931a
Azamjon0898 <98kevin08@gmail.com>
2025-01-03 22:16:45 +0900

    feat: develop getUsers business logic

--- src/controllers/restaurant.controller.ts
+++ src/controllers/restaurant.controller.ts
@@ -103,6 +103,27 @@ restaurantController.logout = async (
   }
 };
 
+restaurantController.getUsers = async (req: Request, res: Response) => {
+  try {
+    console.log("getUsers");
+    const result = await memberService.getUsers();
+    console.log("result:", result);
+
+    res.render("users", { users: result });
+  } catch(err) {
+      console.log("Error, getUsers:", err);
+      res.redirect("/admin/login");
+  }
+};
+
+restaurantController.updateChosenUsen = (req: Request, res: Response) => {
+  try {
+    console.log("updateChosenUsen");
+  } catch(err) {
+    console.log("Error, updateChosenUsen:", err);
+  }
+};
+
 restaurantController.checkAuthSession = async (
   req: AdminRequest, 
   res: Response
