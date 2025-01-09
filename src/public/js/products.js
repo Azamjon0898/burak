5db6945215e2fcc8c259f3e176b9f4032952e047
Azamjon0898 <98kevin08@gmail.com>
2025-01-09 22:04:34 +0900

    feat: create new product frontend logic develop

--- src/public/js/products.js
+++ src/public/js/products.js
@@ -1,1 +1,84 @@
 console.log("Products frontend javascript file");
+console.log("Products frontend javascript file");
+$(function () {
+  $(".product-collection").on("change", () => {
+    const selectedValue = $(".product-collection").val();
+
+    if (selectedValue === "DRINK") {
+      $("#product-collection").hide();
+      $("#product-volume").show();
+    } else {
+      $("#product-volume").hide();
+      $("#product-collection").show();
+    }
+  });
+
+  $("#process-btn").on("click", () => {
+    $(".dish-container").slideToggle(500);
+    $("#process-btn").css("display", "none");
+  });
+  $("#cancel-btn").on("click", function (e) {
+    e.preventDefault();
+    $(".dish-container").slideToggle(100);
+    $("#process-btn").css("display", "flex");
+  });
+
+  $(".new-product-status").on("change", async function (e) {
+    const id = e.target.id,
+      productStatus = $(`#${id}.new-product-status`).val();
+
+    try {
+      const response = await axios.post(`/admin/product/${id}`, {
+        productStatus: productStatus,
+      });
+      const result = response.data;
+      if (result.data) {
+        console.log("Product updated !");
+        $(".new-product-status").blur();
+      } else {
+        console.log("Product updated !");
+      }
+    } catch (err) {
+      console.log(err);
+      alert("Product update failed");
+    }
+  });
+});
+function validateForm() {
+  const productName = $(".product-name").val(),
+    productPrice = $(".product-price").val(),
+    productLeftCount = $(".product-left-count").val(),
+    productCollection = $(".product-collection").val(),
+    productDesc = $(".product-desc").val(),
+    productStatus = $(".product-status").val();
+
+  if (
+    productName === "" ||
+    productPrice === "" ||
+    productLeftCount === "" ||
+    productCollection === "" ||
+    productDesc === "" ||
+    productStatus === ""
+  ) {
+    alert("Please insert all required inputs");
+    return false;
+  } else return true;
+}
+
+function previewFileHandler(input, order) {
+  const imgClasname = input.className,
+    file = $(`.${imgClasname}`).get(0).files[0],
+    fileType = file["type"],
+    validateImageType = ["image/jpg", "image/jpeg", "image/png"];
+  if (!validateImageType.includes(fileType)) {
+    alert("Please insert only jpeg ,jpg and png!");
+  } else {
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = function () {
+        $(`#image-section-${order}`).attr("src", reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+}
